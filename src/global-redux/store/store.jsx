b655663435bf6f25774a9ea780aec57899db9f10
Sardar Ali Murad Ali <120336967+Sardar-Ali-Murad@users.noreply.github.com>
2023-12-12 15:02:23 +0500

    Delete src/global-redux directory

--- src/global-redux/store/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import commonReducer from "../reducers/common/slice";
-import authReducer from "../reducers/auth/slice";
-export const store = configureStore({
-  reducer: {
-    common: commonReducer,
-    auth: authReducer,
-  },
-});
