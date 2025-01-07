c0fba781fd81452f1b4454f3e4c1119e85886424
Sardar-Ali-Murad <mar389786@gmail.com>
2025-01-07 19:29:44 +0500

    tomcat deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/login/",
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true,
+  },
+});
