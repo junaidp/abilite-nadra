37b9788f1fe47c7808bd0b2ba22ea9f37484750b
Sardar-Ali-Murad <mar389786@gmail.com>
2025-07-10 16:06:03 +0500

    fiscal year changes in job scheduling

--- src/components/dashboard/planning/job-scheduling/start-scheduling/components/job-schedule-list/index.jsx
+++ src/components/dashboard/planning/job-scheduling/start-scheduling/components/job-schedule-list/index.jsx
@@ -12,20 +12,20 @@ const JobScheduleList = ({
   const { singleJobSchedulingObject, loading } = useSelector(
     (state) => state?.planningJobScheduling
   );
-  const { user } = useSelector((state) => state?.auth);
+  const { user, userCompany } = useSelector((state) => state?.auth);
 
   function handleChangeDate(event, id) {
     const selectedDate = new Date(event.target.value);
-    const currentDate = new Date();
+    // const currentDate = new Date();
 
-    currentDate.setHours(0, 0, 0, 0);
+    // currentDate.setHours(0, 0, 0, 0);
 
-    if (selectedDate < currentDate) {
-      toast.error(
-        "The planned start date cannot be a past date. Please select today or a future date."
-      );
-      return;
-    }
+    // if (selectedDate < currentDate) {
+    //   toast.error(
+    //     "The planned start date cannot be a past date. Please select today or a future date."
+    //   );
+    //   return;
+    // }
 
     if (
       !singleJobSchedulingObject?.timeAndDateAllocation?.estimatedWeeks ||
@@ -46,12 +46,12 @@ const JobScheduleList = ({
         jobScheduleList: pre?.jobScheduleList?.map((listItem) =>
           listItem?.id === id
             ? {
-                ...listItem,
-                plannedJobStartDate: event.target.value,
-                plannedJobEndDate: selectedDate.setDate(
-                  selectedDate.getDate() + weeksToAdd * 7
-                ),
-              }
+              ...listItem,
+              plannedJobStartDate: event.target.value,
+              plannedJobEndDate: selectedDate.setDate(
+                selectedDate.getDate() + weeksToAdd * 7
+              ),
+            }
             : listItem
         ),
       };
@@ -124,20 +124,23 @@ const JobScheduleList = ({
                         value={
                           list?.plannedJobStartDate
                             ? moment
-                                .utc(list?.plannedJobStartDate)
-                                .format("YYYY-MM-DD")
+                              .utc(list?.plannedJobStartDate)
+                              .format("YYYY-MM-DD")
                             : null
                         }
                         disabled={
                           singleJobSchedulingObject?.locked === true ||
-                          (singleJobSchedulingObject?.complete === true &&
-                            singleJobSchedulingObject?.locked === false &&
-                            user[0]?.userId?.employeeid?.userHierarchy !==
+                            (singleJobSchedulingObject?.complete === true &&
+                              singleJobSchedulingObject?.locked === false &&
+                              user[0]?.userId?.employeeid?.userHierarchy !==
                               "IAH")
                             ? true
                             : false
                         }
                         onChange={(event) => handleChangeDate(event, list?.id)}
+                        min={moment(userCompany.fiscalYearForm).format("YYYY-MM-DD")}
+                        max={moment(userCompany.fiscalYearTo).format("YYYY-MM-DD")}
+
                       />
                       {list?.plannedJobStartDate === null && (
                         <p className="error mt-1">date is required</p>
@@ -154,8 +157,8 @@ const JobScheduleList = ({
                         value={
                           list?.plannedJobEndDate
                             ? moment
-                                .utc(list?.plannedJobEndDate)
-                                .format("YYYY-MM-DD")
+                              .utc(list?.plannedJobEndDate)
+                              .format("YYYY-MM-DD")
                             : null
                         }
                         readOnly
@@ -171,22 +174,21 @@ const JobScheduleList = ({
             (singleJobSchedulingObject?.complete === true &&
               singleJobSchedulingObject?.locked === false &&
               user[0]?.userId?.employeeid?.userHierarchy === "IAH")) && (
-            <div className="row mt-3">
-              <div className="col-lg-12 justify-content-end text-end">
-                <div
-                  className={`btn btn-labeled btn-primary px-3 shadow ${
-                    loading && "disabled"
-                  }`}
-                  onClick={handleSave}
-                >
-                  <span className="btn-label me-2">
-                    <i className="fa fa-check-circle"></i>
-                  </span>
-                  {loading ? "Loading..." : "Save"}
+              <div className="row mt-3">
+                <div className="col-lg-12 justify-content-end text-end">
+                  <div
+                    className={`btn btn-labeled btn-primary px-3 shadow ${loading && "disabled"
+                      }`}
+                    onClick={handleSave}
+                  >
+                    <span className="btn-label me-2">
+                      <i className="fa fa-check-circle"></i>
+                    </span>
+                    {loading ? "Loading..." : "Save"}
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
         </div>
       </div>
     </div>
